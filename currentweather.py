30e06256479aa019b39d8fea1ae532f5b55f0061
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2024-02-21 18:20:59 +0000

    Add files via upload
    Print temperature to console

--- currentweather.py
+++ currentweather.py
@@ -5,8 +5,8 @@ url = "https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&curr
 response = requests.get(url)
 #print(response.json())
 data = response.json()
-with open ("weatherdump.json", "w") as fp:
-    json.dump(data, fp)
+#with open ("weatherdump.json", "w") as fp:
+#    json.dump(data, fp)
 
 tempObject = data["current"]["temperature_2m"]
 
