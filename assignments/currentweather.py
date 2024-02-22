554cb559a6f854e6b3209d2f4de0fe973b7e61a6
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2024-02-22 14:24:08 +0000

    create function getcurrenttemp()

--- assignments/currentweather.py
+++ assignments/currentweather.py
@@ -2,12 +2,14 @@ import requests
 import json
 
 url = "https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&current=temperature_2m"
-response = requests.get(url)
+
 #print(response.json())
-data = response.json()
-#with open ("weatherdump.json", "w") as fp:
-#    json.dump(data, fp)
+def getcurrenttemp():
+    response = requests.get(url)
+    data = response.json()
+    #with open ("weatherdump.json", "w") as fp:
+    #    json.dump(data, fp)
+    tempObject = data["current"]["temperature_2m"]
+    return tempObject
 
-tempObject = data["current"]["temperature_2m"]
-
-print(tempObject)
+print(getcurrenttemp())
